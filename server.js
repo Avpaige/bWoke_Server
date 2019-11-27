42cda0fd7177452de4dc5ba7591dbc93fd735a52
Ari Virgil-Paige <avpaige@gmail.com>
2019-11-26 21:40:51 -0800

    comment out dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+// require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 
 //ALL DEPENDENCIES
 const express = require("express");
