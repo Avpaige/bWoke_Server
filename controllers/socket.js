b14f42f54a75a1fd2e87b4ba9749a36bdb6dcee9
Ari Virgil-Paige <avpaige@gmail.com>
2019-11-26 21:35:09 -0800

    initial files

---
+++ controllers/socket.js
@@ -0,0 +1,31 @@
+module.exports = server => {
+    const io = require('socket.io')(server);
+    const nsp = io.of("/talk")
+    let rooms = []
+
+    nsp.on('connection', function (socket) {
+        socket.emit('connected_success')
+
+        socket.on('room', (room) => {
+            socket.join(room);
+            rooms.push(room);
+
+        });
+
+        socket.on('chat message', (data) => {
+            const { room, message } = data
+            console.log('got message', message);
+            socket.broadcast.to(room).emit('broadcast', message);
+        });
+
+        socket.on('leave room', (room) => {
+            socket.leave(room);
+            var i = rooms.indexOf(room);
+            rooms.splice(i, 1);
+        });
+    });
+
+    io.on('connection', function (socket) {
+        console.log('no namespace connection')
+    })
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
+
+//ALL DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const app = express();
+const morgan = require('morgan')
+app.use(morgan('combined'))
+const server = http.Server(app);
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+require("./controllers/socket")(server);
+const clientDir = path.join(__dirname, "../client");
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+// Express middleware that allows POSTing data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// use the cookie-parser to help with auth token,
+// it must come before the customAuthMiddleware
+app.use(cookieParser());
+
+// serve up the public folder so we can request static
+// assets from the client
+app.use("/assets", express.static(clientDir));
+
+// Start the API server
+server.listen(PORT, function(res, err) {
+	console.log(`🌎  ==> bWoke Server now listening on PORT ${PORT}!`);
+
+	if (err) {
+		console.err("there is an ERROR", err)
+	}
+  });
+
