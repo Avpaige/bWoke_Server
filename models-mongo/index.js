d6710e876d630b17f6e4a1338106542581cc7640
Ari Virgil-Paige <avpaige@gmail.com>
2019-12-04 00:16:35 -0800

    updates to server

--- models-mongo/index.js
+++ models-mongo/index.js
@@ -1,11 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-    Messages: require("./chat"),
-    ChatRoom: require(".chatRooms"),
-=======
     Messages: require("./Chat"),
     ChatRoom: require("./ChatRooms"),
->>>>>>> fbcbd608d7d97c1f429980a3c886e7d3c587f929
     Events: require("./events"),
     Celeb: require("./celebrities"),
    };
