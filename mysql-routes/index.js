d4fc0f418dd3f223aaf04f3168a23dcf8b842a1d
ingan <isabelngan27@gmail.com>
2019-12-04 11:23:29 -0800

    whyyyy

--- mysql-routes/index.js
+++ mysql-routes/index.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const userRoutes = require("./user.js");
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-    host: "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "dq1vocex9imr5voq",
-    password: "q32zx4na7260vbkq",
-    database: "vzqlt1dmmjo19zll"
-});
-connection.connect(err => {
-    if (err) {
-        return err;
-    }
-});
+// const mysql = require("mysql2");
+// const connection = mysql.createConnection({
+//     host: "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "dq1vocex9imr5voq",
+//     password: "q32zx4na7260vbkq",
+//     database: "vzqlt1dmmjo19zll"
+// });
+// connection.connect(err => {
+//     if (err) {
+//         return err;
+//     }
+// });
 
 
 router.use("/", userRoutes);
