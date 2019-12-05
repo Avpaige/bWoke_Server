625b586a5f77df565d4ca2e9e75c6281ee0cc5a4
Ari Virgil-Paige <avpaige@gmail.com>
2019-12-04 18:02:10 -0800

    celeb jsons

---
+++ models-mongo/celeb_seeds.js
@@ -0,0 +1,91 @@
+celebs = [
+    {
+        "celebrity": "Usher",
+        "name": "Boys & Girls Clubs of America",
+        "cause": "Youth Development, Shelter, and Crisis Services",
+        "mission": " Formed in 1906, Boys & Girls Clubs of America (BGCA) is a national organization of local chapters which provide after-school programs for young people. Boys & Girls Clubs annually serve nearly 4 million young people, through membership and community outreach, in some 4,100 Club facilities throughout the country and BGCA-affiliated Youth Centers on U.S. military installations worldwide. The mission is to enable all young people, especially those who need us most, to reach their full potential as productive, caring, responsible citizens, by providing a world-class Club Experience that assures success is within reach of every young person who enters our doors, with all members on track to graduate from high school with a plan for the future, demonstrating good character and citizenship, and living a healthy lifestyle.",
+        "tagline": "http://www.bgca.org",
+        "url": "http://www.bgca.org"
+    }, {
+        "celebrity": "Imagine Dragons",
+        "name": "The Trevor Project",
+        "cause": "Advocacy and Education",
+        "mission": "charity mission here",
+        "tagline" : "Saving Young Lives",
+        "url": "http://www.thetrevorproject.org"
+    },
+    {
+        "celebrity": "Billie Eilish",
+        "name": "Oxfam America",
+        "cause": "Development and Relief Services",
+        "mission": "charity mission here",
+        "tagline" : "Oxfam is a global organization working to end the injustice of poverty. We help people build better futures for themselves, hold the powerful accountable, and save lives in disasters. Our mission is to tackle the root causes of poverty and create lasting solutions",
+        "url": " http://www.oxfamamerica.org"
+    },
+    {
+        "celebrity": "Shawn Mendes",
+        "name": "American Red Cross",
+        "cause": "Multipurpose Human Service Organizations",
+        "mission": "Since its founding in 1881 by visionary leader Clara Barton, the American Red Cross has been the nation's premier emergency response organization. We bring shelter, food and comfort to those affected by disasters, large and small. We collect lifesaving donated blood and supply it to patients in need. We provide support to our men and women in military bases around the world, and to the families they leave behind. We train communities in CPR, first aid and other skills that save lives. And we assist our neighbors abroad with critical disaster response, preparedness and disease prevention efforts. We are able to do all this by mobilizing the power of volunteers and the generosity of donors.",
+        "tagline" : "Helping prevent and alleviate human suffering in the face of emergencies.",
+        "url": "http://www.redcross.org"
+    },
+    {
+        "celebrity": "Michael B. Jordan",
+        "name": "Feeding America",
+        "cause": "Multipurpose Human Service Organizations, Hunger",
+        "mission": "Feeding America is the nationwide network of 200 food banks that leads the fight against hunger in the United States. Together, we provide food to more than 46 million people through 60,000 food pantries and meal programs in communities across America. Feeding America also supports programs that improve food security among the people we serve; educates the public about the problem of hunger; and advocates for legislation that protects people from going hungry. Individuals, charities, businesses and government all have a role in ending hunger. Donate. Volunteer. Advocate. Educate. Together we can solve hunger.",
+        "tagline" : "Connecting people with food and ending hunger through a nationwide network of food banks.",
+        "url": "http://www.feedingamerica.org/"
+    },
+    {
+        "celebrity": "Lili Reinhart",
+        "name": "American Cancer Society",
+        "cause": "cDiseases, Disorders, and Disciplines",
+        "mission": "The American Cancer Society's mission is to save lives, celebrate lives, and lead the fight for a world without cancer.",
+        "tagline" : " Save lives, celebrate lives, and lead the fight for a world without cancer.",
+        "url": "http://www.cancer.org"
+    }, {
+        "celebrity": "Timothée Chalamet",
+        "name": "Rape, Abuse & Incest National Network (RAINN)",
+        "cause": "Social Services",
+        "mission": "RAINN (Rape, Abuse & Incest National Network) is the nation's largest anti-sexual violence organization. RAINN created and operates the National Sexual Assault Hotline (800.656.HOPE, online.rainn.org y rainn.org/es) in partnership with more than 1,000 local sexual assault service providers across the country and operates the DoD Safe Helpline for the Department of Defense. RAINN also carries out programs to prevent sexual violence, help survivors, and ensure that perpetrators are brought to justice.",
+        "tagline" : "Leading the National Effort to End Sexual Violence",
+        "url": "http://www.rainn.org/"
+    }, {
+        "celebrity": "Jesse Williams",
+        "name": "Advancement Project",
+        "cause": "Advocacy and Education",
+        "mission": "Advancement Project is a next generation, multi-racial civil rights organization. Rooted in the great human rights struggles for equality and justice, we exist to fulfill America's promise of a caring, inclusive and just democracy. We use innovative tools and strategies to strengthen social movements and achieve high impact policy change. From Advancement Project's inception, we have worked on the-ground, helping organized communities of color dismantle and reform the unjust and inequitable policies that undermine the promise of democracy. Simultaneously, we have aggressively sought and seized opportunities to promote this approach to racial justice.",
+        "tagline": " …just democracy!",
+        "url": " http://www.advancementproject.org/"
+    }, {
+        "celebrity": "Michael Phelps",
+        "name": "Special Olympics",
+        "cause": "Multipurpose Human Service Organizations",
+        "mission": " Special Olympics is an international organization that changes lives by promoting understanding, acceptance and inclusion between people with and without intellectual disabilities (ID). Through sports, our athletes are seeing themselves for their abilities, not disabilities. Their world is opened with acceptance and understanding. They become confident and empowered by their accomplishments. They are also making new friends, as part of the most inclusive community on the planet -- a global community that is growing every day. Our 4.5 million Special Olympics athletes come from more than 170 countries. The goal of Special Olympics, which was founded in 1968 by Eunice Kennedy Shriver, is to reach out to the almost 200 million people in the world with ID to offer continuing opportunities to realize their potential, develop physical fitness, demonstrate courage and experience joy and friendship.",
+        "tagline" : "Sports that creates inclusive communities throughout the world, ending intolerance, injustice, and inactivity.",
+        "url": "http://www.specialolympics.org"
+    }, {
+        "celebrity": "Megan Rapinoe",
+        "name": "American Civil Liberties Union Foundation",
+        "cause": "Advocacy and Education",
+        "mission": "The American Civil Liberties Union (ACLU) Foundation is the arm of the ACLU that conducts litigation and communication efforts. The Foundation provides legal presentation in cases involving issues of civil liberties and constitutional rights ranging from administrative hearings to trials and appeals. In addition, the Foundation educates the public as to civil liberties and constitutional rights by disseminating literature and other publications. The ACLU Foundation preserves and promotes civil rights and liberties as guaranteed by the United States Constitution.",
+        "tagline" : "We the People",
+        "url": "http://www.aclu.org"
+    }, {
+        "celebrity": "Lebron James",
+        "name": "After-School All-Stars",
+        "cause": "Youth Development, Shelter, and Crisis Services",
+        "mission": "After-School All-Stars provides comprehensive after-school programs that keep children safe and help them succeed in school and life. For more than 20 years, we have offered free, high-quality programs to at-risk youth across the country. Our goals for our All-Stars are that they grow up safe and healthy, graduate high school and go on to college, find careers they love and give back to their communities. We achieve these goals by fueling our students' passions, tying their interests to academic support, enrichment activities and health and fitness programs that instill the confidence and resilience they'll need for success in school and in life. At ASAS, learning is disguised in hands-on, student-directed projects that engage students' imaginations and extend the classroom to their playing fields, neighborhoods, and homes. We help our All-Stars gain the confidence and skills they need to lead their",
+        "tagline" : "After school and summer programs for underserved youth",
+        "url": "http://www.afterschoolallstars.org/"
+    }, {
+        "celebrity": "Serena Williams",
+        "name": "UNICEF USA",
+        "cause": "International Peace, Security, and Affairs",
+        "mission": "The United Nations Children's Fund (UNICEF) works in more than 190 countries and territories to put children first. UNICEF has helped save more children's lives than any other humanitarian organization, by providing health care and immunizations, clean water and sanitation, nutrition, education, emergency relief and more. UNICEF USA supports UNICEF's work through fundraising, advocacy and education in the United States. Together, we are working toward the day when no children die from preventable causes and every child has a safe and healthy childhood.",
+        "tagline" : "Children First",
+        "url": " http://www.unicefusa.org"
+    }, 
+]
